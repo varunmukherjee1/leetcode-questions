53b57afd6277c04cbfc49078c633c45ab4f440c9
Varun <varunmukherjee7@gmail.com>
2022-10-03 00:41:18 +0530

    278

---
+++ 278. First Bad Version/Solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int lo = 1;
+        int hi = n;
+        int mid;
+        int ans = 1;
+
+        while(lo <= hi){
+            mid = lo + (hi - lo)/2;
+
+            if(isBadVersion(mid)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};

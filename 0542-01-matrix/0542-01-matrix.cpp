ca88a8b2c10b82296c5c26e5701bb98cc5901ab8
Varun Mukherjee <87906910+varunmukherjee1@users.noreply.github.com>
2022-11-14 21:19:04 +0530

    Time: 76 ms (96.86%), Space: 30.4 MB (55.83%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,53 +1,52 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>> ans(m,vector<int>(n,-1));
-        queue<pair<int,int>> pq;
+        vector<vector<int>> vec(m,vector<int>(n,-1));
+
+        queue<pair<int,int>> q;
 
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
+                
                 if(mat[i][j] == 0){
-                    ans[i][j] = 0;
-                    pq.push({i,j});
+                    vec[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
 
-        while(!pq.empty()){
-            pair<int,int> temp = pq.front();
-            pq.pop();
+        while(!q.empty()){
 
-            int i = temp.first;
-            int j = temp.second;
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
 
-            if((i+1) < m && ans[i+1][j] == -1){
-                pq.push({i+1,j});
-                ans[i+1][j] = ans[i][j] + 1;
+            if((i+1) < m && vec[i+1][j] == -1){
+                q.push({i+1,j});
+                vec[i+1][j] = vec[i][j] + 1;
             }
 
-            if((j+1) < n && ans[i][j+1] == -1){
-                pq.push({i,j+1});
-                ans[i][j+1] = ans[i][j] + 1;
+            if((j+1) < n && vec[i][j+1] == -1){
+                q.push({i,j+1});
+                vec[i][j+1] = vec[i][j] + 1;
             }
 
-            if((i-1) >= 0 && ans[i-1][j] == -1){
-                pq.push({i-1,j});
-                ans[i-1][j] = ans[i][j] + 1;
+            if((j-1) >= 0 && vec[i][j-1] == -1){
+                q.push({i,j-1});
+                vec[i][j-1] = vec[i][j] + 1;
             }
 
-            if((j-1) >= 0 && ans[i][j-1] == -1){
-                pq.push({i,j-1});
-                ans[i][j-1] = ans[i][j] + 1;
+            if((i-1) >= 0 && vec[i-1][j] == -1){
+                q.push({i-1,j});
+                vec[i-1][j] = vec[i][j] + 1;
             }
         }
 
-        return ans;
+        return vec;
     }
 };

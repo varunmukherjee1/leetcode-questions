e5b2891605d6d538e43be3bae7e0481ffbd4c66d
Varun Mukherjee <87906910+varunmukherjee1@users.noreply.github.com>
2022-11-14 23:47:15 +0530

    Time: 11 ms (72.11%), Space: 13.1 MB (54.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,87 +1,75 @@
 class Solution {
 public:
-
     int orangesRotting(vector<vector<int>>& grid) {
         
+        int ans = 0;
+
+        queue<pair<int,int>> que;
+
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> ans(m,vector<int>(n,0));
-
-        int t = -1;
-
-        queue<pair<int,int>> que;
         int fresh = 0;
 
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                ans[i][j] = grid[i][j];
-                
-                if(grid[i][j] == 1){
+                if(grid[i][j] == 1)
                     fresh++;
-                }
-
-                if(grid[i][j] == 2){
+                
+                if(grid[i][j] == 2)
                     que.push({i,j});
-                }
             }
         }
 
-        // cout<<"fresh = "<<fresh<<endl;
-
-        // bool changed = false;
+        if(fresh == 0)
+            return ans;
 
         while(!que.empty()){
 
             int temp = que.size();
+            int pf = fresh;
 
             while(temp--){
 
-                pair<int,int> p = que.front();
+                int i = que.front().first;
+                int j = que.front().second;
                 que.pop();
 
-                int i = p.first;
-                int j = p.second;
-
-                // changed = false;
-                int init = fresh;
-
-                if(i + 1 < m && ans[i+ 1][j] == 1){
-                    ans[i+1][j] = 2;
+                if((i+1) < m && grid[i+1][j] == 1){
                     que.push({i+1,j});
+                    grid[i+1][j] = 2;
                     fresh--;
                 }
 
-                if(j + 1 < n && ans[i][j+1] == 1){
-                    ans[i][j+1] = 2;
+                if((j+1) < n && grid[i][j+1] == 1){
                     que.push({i,j+1});
+                    grid[i][j+1] = 2;
                     fresh--;
                 }
 
-                if(i - 1 >= 0 && ans[i- 1][j] == 1){
-                    ans[i-1][j] = 2;
-                    que.push({i-1,j});
-                    fresh--;
-
-                }
-
-                if(j - 1 >= 0 && ans[i][j-1] == 1){
-                    ans[i][j-1] = 2;
+                if((j-1) >= 0 && grid[i][j-1] == 1){
                     que.push({i,j-1});
+                    grid[i][j-1] = 2;
+                    fresh--;
+                }
+
+                if((i-1) >= 0 && grid[i-1][j] == 1){
+                    que.push({i-1,j});
+                    grid[i-1][j] = 2;
                     fresh--;
                 }
             }
 
-            t++;
+            if(fresh == pf)
+                break;
+            
+            ans++;
         }
 
         if(fresh > 0){
             return -1;
         }
 
-        if( t == -1)
-            return 0;
-
-        return t;
+        return ans;
     }
 };

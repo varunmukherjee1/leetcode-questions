4e9bd6146ddca0bb7304b640274e6db6d98b2482
Varun Mukherjee <87906910+varunmukherjee1@users.noreply.github.com>
2022-11-17 01:31:29 +0530

    Time: 16 ms (97.47%), Space: 16.8 MB (86.41%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0;
+
+        for(int i : nums){
+            ans ^= i;
+        }
+
+        return ans;
+    }
+};

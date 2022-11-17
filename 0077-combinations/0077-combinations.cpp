de50663adaf3951dfa2daa8bec8a293b98b41461
Varun Mukherjee <87906910+varunmukherjee1@users.noreply.github.com>
2022-11-18 01:43:29 +0530

    Time: 964 ms (9.11%), Space: 164.2 MB (15.47%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
+    void helper(int id , int n, int k , vector<int> temp , vector<vector<int>> &ans){
+        if(temp.size() == k){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = id ; i <= n ; i++){
+            temp.push_back(i);
+            helper(i+1,n,k,temp,ans);
+            temp.pop_back();
+        }
+    }
 
     vector<vector<int>> combine(int n, int k) {
-        
         vector<vector<int>> ans;
+        vector<int> temp;
 
-        vector<int> vec(k,0);
-        int i = 0;
-
-        while(i >= 0){
-            
-            vec[i]++;
-
-            if(vec[i] > n){
-                --i;
-            }
-            else if(i == k-1){
-                ans.push_back(vec);
-            }
-            else{
-                i++;
-                vec[i] = vec[i-1];
-            }
-        }
+        helper(1,n,k,temp,ans);
 
         return ans;
     }
